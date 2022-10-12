bc05cc027b78dd1656e2df327e4ab56fa9778bd0
Dennis Koopman <dennis.koopman@fizor.com>
2022-10-12 11:33:47 +0200

    Add Tasks and Task components. Add delete and doubleclick logic for Tasks. Upto 1:03 in the video

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [myTasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shoping",
+      day: "Feb 7th at 4:00pm",
+      reminder: false,
+    },
+  ]);
+
+  // delete Task
+  const deleteTask = (id) => {
+    setTasks(myTasks.filter((task) => task.id !== id));
+  };
+
+  // toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      myTasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      {myTasks.length > 0 ? (
+        <Tasks
+          tasks={myTasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+        />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
